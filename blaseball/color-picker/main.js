a683c13c50bd9810bf83472a18757f4130540e2b
Chris <eltrov@gmail.com>
2020-09-12 20:46:39 -0500

    lots of stuff
    adding color-picker and fantasy-leage stuff

---
+++ blaseball/color-picker/main.js
@@ -0,0 +1,25 @@
+function main() {
+    var br = document.createElement("br");
+    br.className = "break";
+
+    var count = 9780;
+
+
+    for (i = 0; i < count; i++) {
+        var code = makeCode();
+        var teamLogo = document.createElement("div");
+        teamLogo.className = "team-logo";
+        teamLogo.style = "background-color:" + code + ";";
+        teamLogo.innerText = "🧠";
+        (document.getElementById("palette")).appendChild(teamLogo)
+    }
+}
+
+function makeCode() {
+    var makeColorCode = '0123456789ABCDEF';
+    var code = '#';
+    for (var count = 0; count < 6; count++) {
+        code = code + makeColorCode[Math.floor(Math.random() * 16)];
+    }
+    return code;
+}
